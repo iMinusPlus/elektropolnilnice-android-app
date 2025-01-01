2751190ec5fa6b2160cd255f68f95415c228429c
davidgole1 <david.gole1@student.um.si>
2025-01-01 18:02:12 +0100

    S fragmenti narejena navigacijska vrstica

--- app/src/main/java/com/example/elektropolnilnice_app/MainActivity.kt
+++ app/src/main/java/com/example/elektropolnilnice_app/MainActivity.kt
@@ -1,20 +1,39 @@
 package com.example.elektropolnilnice_app
 
+import MapFragment
+import ProfileFragment
+import RecordFragment
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Default fragment
+        loadFragment(MapFragment())
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        bottomNavigationView.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.nav_map -> loadFragment(MapFragment())
+                R.id.nav_record -> loadFragment(RecordFragment())
+                R.id.nav_profile -> loadFragment(ProfileFragment())
+                else -> false
+            }
+            true
         }
+    }
+
+    private fun loadFragment(fragment: Fragment): Boolean {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
+        return true
     }
 }
