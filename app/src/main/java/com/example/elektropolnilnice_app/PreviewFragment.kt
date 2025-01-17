c4d17d32c528cb3f38c52b0eac2767e08495e010
NucleusBeast <36455332+NucleusBeast@users.noreply.github.com>
2025-01-17 08:31:56 +0100

    PMI-91 Added preview fragment

--- app/src/main/java/com/example/elektropolnilnice_app/PreviewFragment.kt
+++ app/src/main/java/com/example/elektropolnilnice_app/PreviewFragment.kt
@@ -1,12 +1,15 @@
 package com.example.elektropolnilnice_app
 
+import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import java.io.IOException
 
 class PreviewFragment : Fragment() {
 
@@ -25,8 +28,24 @@ class PreviewFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_preview, container, false)
         val imageView = view.findViewById<ImageView>(R.id.previewImageView)
+        val resultTextView: TextView = view.findViewById<TextView>(R.id.test)
         imageUri?.let {
             imageView.setImageURI(it)
+
+            try {
+                // Load the test image from assets
+                val inputStream = requireContext().contentResolver.openInputStream(it)//assets.open("test_image.jpg") // Replace with your image name
+                val bitmap = BitmapFactory.decodeStream(inputStream)
+                resultTextView.text = "Error loading model or image"
+
+//                // Run the model and display the result
+//                val prediction = classifier.predict(bitmap)
+//                resultTextView.text = "Prediction: $prediction"
+
+            } catch (e: IOException) {
+                e.printStackTrace()
+                resultTextView.text = "Error loading model or image"
+            }
         }
         return view
     }
